8af4f03923e2ffd68bcb48de591ad3772ab05f9e
xxyoon2 <103083213+xxyoon2@users.noreply.github.com>
2022-06-10 17:01:42 +0900

    Create Solution_02.c

---
+++ Solution_02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int multiplierOf3 = 1;
+
+    while (multiplierOf3 < 1000)
+    {
+        printf("%d\n", multiplierOf3);
+        multiplierOf3 *= 3;
+    }
+
+}
